9e81af3ba8093f9970a2e66b6fb1d50cd08b563e
Roxana Cretu <cretu.roxanna@gmail.com>
2021-03-31 11:20:16 +0300

    studied variables

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the Racing Game!" );
+
+        Car carReference = new Car();
+        carReference.make = "Skoda";
+        carReference.model = "Fabia";
+        carReference.mileage = 7.5;
+        carReference.fuelLevel = 60;
+        carReference.color = "red";
+        carReference.maxSpeed = 300;
+
+        Engine engine1 = new Engine();
+        engine1.manufacturer = "VW";
+        engine1.capacity = 1900;
+
+        carReference.engine = engine1;
     }
 }
