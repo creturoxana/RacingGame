2f4e9efff29af96b44b1dc52d542917082197fc0
Roxana Cretu <cretu.roxanna@gmail.com>
2021-04-20 17:21:47 +0300

    studied arrays and classic for loops

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,8 +7,8 @@ public class App
 {
     public static void main( String[] args )
     {
-//       Game game = new Game();
-//       game.start();
+       Game game = new Game();
+       game.start();
 
 //      //polymorphism
 //      Vehicle vehicle = new CheatingVehicle();
