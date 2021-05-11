ff54fabb44d0c6006a49a6c047033741507c79f4
Roxana Cretu <cretu.roxanna@gmail.com>
2021-05-11 15:14:39 +0300

    handled null values in MobileComparator

--- src/main/java/org/example/domain/MobileComparator.java
+++ src/main/java/org/example/domain/MobileComparator.java
@@ -7,9 +7,18 @@ public class MobileComparator implements Comparator<Mobile> {
 
   @Override
   public int compare(Mobile o1, Mobile o2) {
+    //both variables are null or contain references to the same object
+      if (o1 == o2) {
+        return 0;
+      }
 
-      //todo: handle null values
+      if (o1 == null) {
+        return -1;
+      }
 
+    if (o2 == null) {
+      return 1;
+    }
     return Double.compare(o1.getTotalTraveledDistance(), o2.getTotalTraveledDistance());
   }
 }
